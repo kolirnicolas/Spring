898b53310b9821d1edcecd7a7b228d536d79d3f7
nicolas <kolirzet09@gmail.com>
2022-05-19 16:46:44 +0300

    Task3

--- src/main/java/com/example/demo/CreateBigBike.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface CreateBigBike {
-    void createBigBike();
-}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,18 @@
 package com.example.demo;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 @AllArgsConstructor
 public class DemoApplication {
-    private final BigBike bigBike;
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Config.class);
+        String str =  annotationConfigApplicationContext.getBean(String.class);
+        System.out.println(str);
     }
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void bigBikeMethod() {
-        bigBike.implementation();
-    }
+
 }
